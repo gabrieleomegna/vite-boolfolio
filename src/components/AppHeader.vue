1c9938f4f261f7ed787bddcb18c556d993568022
gabrieleomegna <gabrieleomegna01@gmail.com>
2024-02-26 18:40:22 +0100

    Implemented AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <header class="container">
+        <h1>Header</h1>
+    </header>
+</template>
+
+<style lang="scss">
+    
+</style>
