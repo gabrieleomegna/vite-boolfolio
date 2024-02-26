e13ba15e7c242f743cd6b2f3e031570aec2e399a
gabrieleomegna <gabrieleomegna01@gmail.com>
2024-02-26 18:40:08 +0100

    connected components AppHeader and AppMain

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain
+  }
+}
+</script>
+
+<template>
+  <AppHeader/>
+  <AppMain/>
+
+</template>
+
+<style lang="scss">
+@use './style/general.scss' as *;
+</style>
