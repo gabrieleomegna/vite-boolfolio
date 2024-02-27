dfdbb3621ba6f47ad8a6701dfa1e84524b4a8b69
gabrieleomegna <gabrieleomegna01@gmail.com>
2024-02-27 17:40:19 +0100

    implemeted HomePage component

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+        <div class="container">
+            <h1>Homepage</h1>
+        </div>
+    </main>
+</template>
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
