0a343d674a503e1f4fea2302cd635df864640d0b
gabrieleomegna <gabrieleomegna01@gmail.com>
2024-02-27 17:40:02 +0100

    modified AppHeader with links that takes to the routes projects.index and home

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,37 @@
+<template>
+    <header class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="">Boolfolio</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active" v-for="(link, index) in linkItems" :key="index">
+                        <router-link class="nav-link" :to="{name: link.routeName}"> {{ link.label }} </router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
 <script>
 export default {
-    
+    name: 'AppHeader',
+    data() {
+        return {
+            linkItems: [
+                {
+                    label: 'Home',
+                    routeName: 'home'
+                },
+                {
+                    label: 'Project',
+                    routeName: 'projects.index'
+                },
+            ]
+        }
+    },
 }
 </script>
 
-<template>
-    <header class="container">
-        <h1>Header</h1>
-    </header>
-</template>
-
-<style lang="scss">
+<style lang="scss" scoped>
     
 </style>
